eb2844f48f272c1f6d93513dfefa1dcd9870c0aa
sanjayjingle <37142093+sanjayjingle@users.noreply.github.com>
2018-11-15 16:11:55 +0530

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,6 +1,8 @@
 <?php
+/////////////////////////////////////////////////////
 include("dbconfig.php");
 $username='sanjayp';
 $name='Huzoor Bux';
+
 $sql=mysql_query($connect,"CALL insert('$username','$name')");
 ?>
